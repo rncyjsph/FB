d36d57f86a2353fab666163d9d1fc7427c72540e
rncyjsph <rncyjsph@gmail.com>
2019-06-07 15:01:32 +0530

    java code

---
+++ src/Firstclass.java
@@ -0,0 +1,10 @@
+
+public class Firstclass {
+//to run first java code
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first java code");
+
+	}
+
+}
